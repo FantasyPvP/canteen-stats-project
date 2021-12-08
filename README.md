737d7301ffcc575ba744d6709dcf217c46fb5d2a
FantasyPvP <harry.blaise.irving@gmail.com>
2021-12-08 22:43:04 +0000

    started the project
    started the project

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# canteen stats project
- 
+
+
+PROJECT UPDATE LOG (please enter a log every time you update the google drive with a file)
+
+
+
+
+08.12.21:
+
+    - created grapher tool concept
+    - created google drive for project
+    - created README file
+    - experimented with graph making functions in the matplotlib python package
+        - use "pip install matplotlib" in command promt to install and "import matplotlib" to import
+        - documentation for the module ----->  https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
+        - used the module to create basic grapher where you can import the x and y values to plot in a bar chart
+
---
+++ graph.py
@@ -0,0 +1,32 @@
+
+import statistics
+import math
+import time
+import random
+import matplotlib.pyplot as grapher
+
+
+
+def barChart(keys, values):
+    for i in range(len(keys)):
+        print(keys[i] + ( " " * ( 20 -  len(keys[i]) ) ) + "|   " + ("-" * values[i])) 
+
+
+
+
+
+positions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+keys = ["a","b","c","pasta king","e","f","g","h","i","j","a","b","c","d","e","f","g","h","i","j"]
+values = []
+
+for i in range(20):
+    values.append(random.randint(0,50))
+
+grapher.bar(
+    positions, values, tick_label = keys, width = 0.8, color = ["red", "blue"]
+    )
+
+grapher.xlabel("item")
+grapher.ylabel("frequency")
+grapher.title("frequency of meals sold in the canteen")
+grapher.show()
